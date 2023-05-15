93a555191db83b9ec4cea9c0a26e8546fad94ee7
Tiago Amaral <tiagofly@outlook.com>
2023-05-15 01:09:37 -0300

    Defined layout cards class. Implementation card factory

---
+++ source/app/Shared/Component/Cards/CardRepoListItemView.swift
@@ -0,0 +1,13 @@
+//
+//  CardRepoListView.swift
+//  github-person
+//
+//  Created by Tiago Amaral on 15/05/23.
+//  Copyright © 2023 developer_organization_name. All rights reserved.
+//
+
+import UIKit
+
+class CardRepoListItemView: UIView {
+    
+}
---
+++ source/app/Shared/Component/Cards/CardUserProfileView.swift
@@ -0,0 +1,9 @@
+//
+//  CardUserProfileView.swift
+//  github-person
+//
+//  Created by Tiago Amaral on 15/05/23.
+//  Copyright © 2023 developer_organization_name. All rights reserved.
+//
+
+import Foundation
