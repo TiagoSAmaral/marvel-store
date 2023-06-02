a9c1b69c8709d2a6606b2c1038051241df186625
Tiago Amaral <tiagofly@outlook.com>
2023-06-02 13:27:24 -0300

    Update project.pbxproj and NetworkErrorTests.swift Implementado teste para tratamento de erros de requisicao.

---
+++ source/test/Shared/Network/NetworkErrorTests.swift
@@ -0,0 +1,43 @@
+//
+//  NetworkErrorTests.swift
+//  marvel-storeTests
+//
+//  Created by Tiago Amaral on 02/06/23.
+//  Copyright © 2023 developer_organization_name. All rights reserved.
+//
+
+import XCTest
+@testable import Marvel_Store_Dev
+
+final class NetworkErrorTests: XCTestCase {
+    
+    var networkErrorTarget: NetworkError?
+    
+    let OFFLINE_MESSAGE_TEST = LocalizedText.with(tagName: .networkOffline)
+    let BADREQUEST_MESSAGE_TEST = LocalizedText.with(tagName: .networkErrorNotDefined)
+    let SERVERNOTRESPONSE_MESSAGE_TEST = LocalizedText.with(tagName: .serverNotResponse)
+    let NOTDEFINED_MESSAGE_TEST = LocalizedText.with(tagName: .networkErrorNotDefined)
+    override func tearDown() {
+        networkErrorTarget = nil
+    }
+
+    func testStatusOffline() {
+        networkErrorTarget = NetworkError.makeError(with: -1)
+        XCTAssertEqual(networkErrorTarget?.message, OFFLINE_MESSAGE_TEST, "Expect same message")
+    }
+    
+    func testStatus400At499() {
+        networkErrorTarget = NetworkError.makeError(with: 400)
+        XCTAssertEqual(networkErrorTarget?.message, BADREQUEST_MESSAGE_TEST, "Expect same message")
+    }
+    
+    func testStatusAbove499() {
+        networkErrorTarget = NetworkError.makeError(with: 500)
+        XCTAssertEqual(networkErrorTarget?.message, SERVERNOTRESPONSE_MESSAGE_TEST, "Expect same message")
+    }
+    
+    func testStatusNotDefined() {
+        networkErrorTarget = NetworkError.makeError(with: nil)
+        XCTAssertEqual(networkErrorTarget?.message, NOTDEFINED_MESSAGE_TEST, "Expect same message")
+    }
+}
