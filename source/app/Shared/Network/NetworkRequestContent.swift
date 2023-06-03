3919c147f0ee56ea0b5dc66e41f72603938d0c6d
Tiago Amaral <tiagofly@outlook.com>
2023-06-03 01:39:22 -0300

    Update NetworkRequestContent.swift Refactor para acompanhar as mudans de URLPathBuilder, também foi feito ajuste para reuso de implementação de métodos e resuzir interface para somente um método.

--- source/app/Shared/Network/NetworkRequestContent.swift
+++ source/app/Shared/Network/NetworkRequestContent.swift
@@ -13,57 +13,53 @@ import Foundation
 typealias GeneralResultHandler = ((Result<GeneralResult<Comic>, NetworkError>) -> Void)
 
 protocol NetworkContentOperation {
-    func requestContentWith(search content: String?, filter: Int?, page: Int?, handler: GeneralResultHandler?)
-    func requestDetail(identifier: Int?, params: [String: Any]?, handler: GeneralResultHandler?)
+    func requestContentWith(param: NetworkRequestParameters, handler: GeneralResultHandler?)
 }
 
 class NetworkRequestContent: NetworkContentOperation, NetworkConectable {
     
-    func requestContentWith(search content: String?, filter: Int?, page: Int?, handler: GeneralResultHandler?) {
+    func requestContentWith(param: NetworkRequestParameters, handler: GeneralResultHandler?) {
         
-        var apiPath: String?
+        let urlPathBuilder: URLPathBuildable = URLPathBuilder(apiSecurer: APISecurity())
         
-        if content != nil {
-            apiPath = ApiRoutes.shared.path(.getSearch(content: content, filter: filter, page: page))
-        } else {
-            apiPath =  ApiRoutes.shared.path(.getListStore(filter: filter, page: page))
-        }
- 
-        let request: RequestApi = RequestApi(urlPath: apiPath, method: Alamofire.HTTPMethod.get)
-        handlerRequest(data: request, resultType: GeneralResult<Comic>.self, layout: .listContentLayoutCard, handler: handler)
-    }
-
-    func requestDetail(identifier: Int?, params: [String: Any]?, handler: GeneralResultHandler?) {
-        let apiPath = ApiRoutes.shared.path(.getDetailWith(identifier))
-        let request: RequestApi = RequestApi(urlPath: apiPath, method: Alamofire.HTTPMethod.get)
-        handlerRequest(data: request, resultType: GeneralResult<Comic>.self, layout: .detailContentLayoutCard, handler: handler)
+        let apiPath = urlPathBuilder.makeUrlWith(identifier: param.identifier,
+                                                 search: param.search,
+                                                 filterSince: param.filterSinceYear,
+                                                 startFrom: param.sincePage)
+        
+        let request: RequestApi = RequestApi(urlPath: apiPath,
+                                             method: Alamofire.HTTPMethod.get)
+        handlerRequest(data: request,
+                       resultType: GeneralResult<Comic>.self,
+                       layout: param.layoutView,
+                       handler: handler)
     }
     
-    func handlerRequest(data: RequestApi, resultType: GeneralResult<Comic>.Type, layout: LayoutView, handler: GeneralResultHandler?) {
+    func handlerRequest(data: RequestApi, resultType: GeneralResult<Comic>.Type, layout: LayoutView?, handler: GeneralResultHandler?) {
         
-        networkRequest(data: data, resultType: resultType) { result in
+        networkRequest(data: data, resultType: resultType) { [weak self] result in
             switch result {
-            case .success(let genericResponse):
+            case .success(var genericResponse):
                 
                 guard let items = genericResponse.data?.results else {
                     handler?(.failure(NetworkError.makeError(with: nil, description: nil)))
                     return
                 }
-                
-                let itemsWithLayout: [Comic] = items.compactMap { (item: Comic) -> Comic in
-                    var mutableItem = item
-                    mutableItem.layout = layout
-                    return mutableItem
-                }
-                
-                var mutableResult = genericResponse
-                mutableResult.data?.results = itemsWithLayout
-                
-                handler?(.success(mutableResult))
+                genericResponse.data?.results = self?.defineLayout(at: items, layoutView: layout)
+                handler?(.success(genericResponse))
                 
             case .failure(let error):
                 handler?(.failure(error))
             }
         }
+    }
+    
+    func defineLayout(at items: [Comic], layoutView: LayoutView?) -> [Comic] {
+        let itemsWithLayout: [Comic] = items.compactMap { (item: Comic) -> Comic in
+            var mutableItem = item
+            mutableItem.layout = layoutView
+            return mutableItem
+        }
+        return itemsWithLayout
     }
 }
