06351f5e7d9f37057ed587f3aad587639ca358f0
Tiago Amaral <tiagofly@outlook.com>
2023-06-03 16:26:17 -0300

    Update ViewMosaicComposer.swift IMplementado view base com composição por stackview

---
+++ source/app/Shared/Component/ViewMosaicComposer/ViewMosaicComposer.swift
@@ -0,0 +1,38 @@
+//
+//  ViewComposer.swift
+//  marvel-store
+//
+//  Created by Tiago Amaral on 03/06/23.
+//  Copyright © 2023 developer_organization_name. All rights reserved.
+//
+
+import UIKit
+
+class ViewMosaicComposer {
+    
+    lazy var baseView: UIView = {
+        var view = UIView()
+        view.backgroundColor = .red //ColorAsset.cardBackgroundColor
+        return view
+    }()
+    
+    lazy var vStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+//        stackView.distribution = .fill
+        return stackView
+    }()
+    
+    init() {
+        makeConstraint()
+    }
+    
+    func insertNew(view: UIView) {
+        vStackView.addArrangedSubview(view)
+    }
+    
+    func makeConstraint() {
+        baseView.addSubviews([vStackView])
+        vStackView.edgeToSuperView()
+    }
+}
