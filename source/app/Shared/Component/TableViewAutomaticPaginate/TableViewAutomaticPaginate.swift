1e0ea7b79bd9bf056681104c961057a14efb322d
Tiago Amaral <tiagofly@outlook.com>
2023-06-03 16:26:30 -0300

    Update TableViewAutomaticPaginate.swift Implementado compoente de tableView.

--- source/app/Shared/Component/TableViewAutomaticPaginate/TableViewAutomaticPaginate.swift
+++ source/app/Shared/Component/TableViewAutomaticPaginate/TableViewAutomaticPaginate.swift
@@ -8,8 +8,20 @@
 
 import UIKit
 
-class TableViewAutomaticPaginate: UITableView, UITableViewDelegate {
+protocol Controller: AnyObject {
+    var view: UIView! { get set }
+    var dataHandler: ListDataHandler? { get }
+}
 
+protocol ListUpdateEvent {
+    func reloadView()
+}
+
+class TableViewAutomaticPaginate: UITableView, UITableViewDelegate, UITableViewDataSource, ListUpdateEvent {
+
+    var dataHandler: ListDataHandler?
+    var cardFactory: CardFactory?
+    
     override init(frame: CGRect, style: UITableView.Style) {
         super.init(frame: frame, style: style)
         defaultSetup()
@@ -21,17 +33,19 @@ class TableViewAutomaticPaginate: UITableView, UITableViewDelegate {
     }
     
     private var isAllowEmitScrollingHasComeToEnt: Bool = true
-    
     private var propagateEvent: (() -> Void)?
     
     private func defaultSetup() {
         delegate = self
+        dataSource = self
         separatorStyle = .none
         separatorInset = .zero
         allowsSelection = false
         rowHeight = UITableView.automaticDimension
+        registerEventScrollDidEnd()
+        registerTableViewCell()
+        makePullToRefresh()
     }
-    
     
     func scrollingHasComeToEnd() {
         isAllowEmitScrollingHasComeToEnt = false
@@ -52,5 +66,61 @@ class TableViewAutomaticPaginate: UITableView, UITableViewDelegate {
                 scrollingHasComeToEnd()
             }
         }
+    }
+    
+    func makePullToRefresh() {
+        let refreshControl = UIRefreshControl()
+        refreshControl.attributedTitle = NSAttributedString(string: LocalizedText.with(tagName: .pullToRefreshText))
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        self.refreshControl = refreshControl
+    }
+    
+    @objc func refresh() {
+        dataHandler?.updateContent()
+    }
+    
+    func registerTableViewCell() {
+        register(GenericTableViewCell.self, forCellReuseIdentifier: GenericTableViewCell.identifier)
+    }
+    
+    func reloadView() {
+        reloadData()
+        refreshControl?.endRefreshing()
+        allowEmitScrollingHasComeToEnd()
+    }
+    
+    func registerEventScrollDidEnd() {
+        register() { [weak self] in
+            self?.dataHandler?.nextPage()
+        }
+    }
+    
+// MARK: - UITableViewDataSource Implementation
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        dataHandler?.numberOfSections() ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        dataHandler?.numberOfItemsBy(section: section) ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: GenericTableViewCell.identifier,
+                                                       for: indexPath) as? GenericTableViewCell,
+              let model = dataHandler?.dataBy(indexPath: indexPath) else {
+            return UITableViewCell()
+        }
+        
+        let card = cardFactory?.makeCard(from: model as? ViewModelBehavior)
+        card?.defineLayout(with: cell.contentView)
+        return cell
+    }
+    
+    // MARK: YearFilterDelegate
+    
+    func receive(value: Int?) {
+        dataHandler?.receiveFilter(value: value)
     }
 }
