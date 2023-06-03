f3ccfc61d376e929b60cd891be4c7add860f4d1f
Tiago Amaral <tiagofly@outlook.com>
2023-06-03 16:29:45 -0300

    Update ViewHandlerEventUpdate.swift Implementado interface para views que precisam ser atualziadas.

---
+++ source/app/Interfaces/ViewHandlerEventUpdate.swift
@@ -0,0 +1,13 @@
+//
+//  ControllerViewHandlerEventUpdate.swift
+//  marvel-store
+//
+//  Created by Tiago Amaral on 03/06/23.
+//  Copyright © 2023 developer_organization_name. All rights reserved.
+//
+
+import UIKit
+
+protocol ViewHandlerEventUpdate where Self: UIView {
+    var updateContentHandler: (() -> Void)? { get set }
+}
