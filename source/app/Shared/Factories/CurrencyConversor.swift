ce3aa526e0251109899183e71ef68fbd58f1b8c5
Tiago Amaral <tiagofly@outlook.com>
2023-06-05 05:00:05 -0300

    Update CurrencyConversor.swift Criado formatador de moeda

---
+++ source/app/Shared/Factories/CurrencyConversor.swift
@@ -0,0 +1,21 @@
+//
+//  CurrencyConversor.swift
+//  marvel-store
+//
+//  Created by Tiago Amaral on 05/06/23.
+//  Copyright © 2023 developer_organization_name. All rights reserved.
+//
+
+import Foundation
+
+enum CurrencyConversor {
+    static func formatFrom(value: Double?) -> String? {
+        guard let value = value else {
+            return nil
+        }
+        let formatter = NumberFormatter()
+        formatter.locale = Locale.current
+        formatter.numberStyle = .currency
+        return formatter.string(from: value as NSNumber)
+    }
+}
