a559167a8ccf352aae16af7f6a02fd02c400ca12
Tiago Amaral <tiagofly@outlook.com>
2023-06-04 21:43:18 -0300

    Update ListCartPageCoordinator.swift, ListCartPageFactory.swift, ListFavoritePageCoordinator.swift, and 1 more file Classe base para demais telas.

---
+++ source/app/Pages/Cart/ListCartPageFactory.swift
@@ -0,0 +1,9 @@
+//
+//  ListCartPageFactory.swift
+//  marvel-store
+//
+//  Created by Tiago Amaral on 04/06/23.
+//  Copyright © 2023 developer_organization_name. All rights reserved.
+//
+
+import Foundation
