483288534782fcdc86855ca685cf8cca7aed815e
Tiago Amaral <tiagofly@outlook.com>
2023-06-05 18:59:23 -0300

    Update Comics.swift Refactor em nome de proprieadades e adicao de métodos para remover item da lista de favoritos e lista de carrinho.

--- source/app/Shared/Model/Comics.swift
+++ source/app/Shared/Model/Comics.swift
@@ -10,22 +10,22 @@ import Foundation
 import RealmSwift
 
 class Comic: Object, Codable, ViewModelBehavior, Model {
-
-    @Persisted(primaryKey: true) var internalId: ObjectId?
     @Persisted var identifier: Int?
     @Persisted var issueNumber: Double?
     @Persisted var thumbnail: ComicImage?
     @Persisted var title: String?
     @Persisted var resume: String?
     @Persisted var layout: LayoutView?
-    @Persisted var isPurchasable: Bool
+    @Persisted var isIntoCart: Bool
     @Persisted var isFavorable: Bool
     @Persisted var images = List<ComicImage>()
     @Persisted var prices = List<ComicPrice>()
     
     var selectAction: ((Model?) -> Void)?
-    var purchaseAction: ((Model?) -> Void)?
-    var favoriteAction: ((Model?) -> Void)?
+    var addCartAction: ((Model?) -> Void)?
+    var removeCartAction: ((Model?) -> Void)?
+    var addFavoriteAction: ((Model?) -> Void)?
+    var removeFavoriteAction: ((Model?) -> Void)?
     
     enum CodingKeys: String, CodingKey {
         case issueNumber, thumbnail, title
