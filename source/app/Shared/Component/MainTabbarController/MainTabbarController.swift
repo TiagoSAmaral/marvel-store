695653cda5e7ded8b6614df8769372a07e67e5a7
Tiago Amaral <tiagofly@outlook.com>
2023-06-05 01:49:03 -0300

    Update MainTabbarController.swift and MainTabbarFactory.swift Implementado MainTabbar para exibir as demais telas junto a tela principal

---
+++ source/app/Shared/Component/MainTabbarController/MainTabbarController.swift
@@ -0,0 +1,48 @@
+//
+//  Tabbar.swift
+//  marvel-store
+//
+//  Created by Tiago Amaral on 04/06/23.
+//  Copyright © 2023 developer_organization_name. All rights reserved.
+//
+
+import UIKit
+
+final class MainTabbarController: UITabBarController {
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        fixBartintColoriOS15()
+        defineApperanceText()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        fixBartintColoriOS15()
+        defineApperanceText()
+    }
+    
+    private func fixBartintColoriOS15() {
+        if #available(iOS 15, *) {
+            let appearance = UITabBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = ColorAsset.cardBackgroundColor
+            tabBar.standardAppearance = appearance
+            tabBar.scrollEdgeAppearance = tabBar.standardAppearance
+        }
+    }
+    
+    private func defineApperanceText() {
+        tabBar.isTranslucent = false
+        let tabBarAppearance = UITabBarAppearance()
+        let tabBarItemAppearance = UITabBarItemAppearance()
+        tabBarItemAppearance.normal.titleTextAttributes = [NSAttributedString.Key.foregroundColor: ColorAsset.descriptionColor ?? .gray]
+        tabBarItemAppearance.selected.titleTextAttributes = [NSAttributedString.Key.foregroundColor: ColorAsset.titleColor ?? .red]
+
+        tabBarAppearance.stackedLayoutAppearance = tabBarItemAppearance
+        tabBar.standardAppearance = tabBarAppearance
+        if #available(iOS 15, *) {
+            tabBar.scrollEdgeAppearance = tabBarAppearance
+        }
+    }
+}
---
+++ source/app/Shared/Component/MainTabbarController/MainTabbarFactory.swift
@@ -0,0 +1,66 @@
+//
+//  MainTabbarFactory.swift
+//  marvel-store
+//
+//  Created by Tiago Amaral on 04/06/23.
+//  Copyright © 2023 developer_organization_name. All rights reserved.
+//
+
+import UIKit
+
+struct MainTabbarFactory {
+    static func makePage() -> UIViewController {
+            
+        let tabbar = MainTabbarController()
+        var viewControllers: [UIViewController] = []
+        
+        if let storageFlow = MainTabbarFactory.defineStorageFlow() {
+            viewControllers.append(storageFlow)
+        }
+        
+        if let favoriteFlow = MainTabbarFactory.defineFavoriteFlow() {
+            viewControllers.append(favoriteFlow)
+        }
+        
+        if let cartFlow = MainTabbarFactory.defineCartFlow() {
+            viewControllers.append(cartFlow)
+        }
+        
+        tabbar.viewControllers = viewControllers
+        
+        return tabbar
+    }
+    
+    private static func defineStorageFlow() -> UIViewController? {
+        let navigation = NavigationController()
+        let coordinator = ListContentPageCoordinator(navigation: NavigationController())
+        guard let controller = ListContentPageFactory.makePage(coordinator: coordinator, model: nil) else {
+            return nil
+        }
+        navigation.tabBarItem.title = LocalizedText.with(tagName: .storeTitle)
+        navigation.viewControllers = [controller]
+        return navigation
+    }
+    
+    private static func defineFavoriteFlow() -> UIViewController? {
+        let navigation = NavigationController()
+        let coordinator = ListFavoritePageCoordinator(navigation: NavigationController())
+        guard let controller = ListFavoritePageFactory.makePage(coordinator: coordinator, model: nil) else {
+            return nil
+        }
+        navigation.title = LocalizedText.with(tagName: .favoriteTitle)
+        navigation.viewControllers = [controller]
+        return navigation
+    }
+    
+    private static func defineCartFlow() -> UIViewController? {
+        let navigation = NavigationController()
+        let coordinator = CartPageCoordinator(navigation: NavigationController())
+        guard let controller = CartPageFactory.makePage(coordinator: coordinator, model: nil) else {
+            return nil
+        }
+        navigation.title = LocalizedText.with(tagName: .cartTitle)
+        navigation.viewControllers = [controller]
+        return navigation
+    }
+}
