aeedb9ef437121876553149a46bd2519a54d1f5f
Tiago Amaral <tiagofly@outlook.com>
2023-06-05 19:02:47 -0300

    Update CartPageFactory.swift Implementado fabrica de lista de items no carrinho.

--- source/app/Pages/Cart/CartPageFactory.swift
+++ source/app/Pages/Cart/CartPageFactory.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class CartPageFactory: FactoryPage {
     static func makePage(coordinator: Coordinator?, model: Model?) -> UIViewController? {
-        UIViewController()
+        let controller = ListContentPageController()
+        let viewModel = ListContentPageViewModel(controller: controller,
+                                               network: nil,
+                                               coordinator: coordinator)
+        viewModel.currentViewModelStrategy = .cart
+        let mosaicComposerView = ViewMosaicComposer()
+        
+        let listView = TableViewAutomaticPaginate()
+        listView.dataHandler = viewModel
+        listView.cardFactory = CardMaker()
+        mosaicComposerView.insertNew(view: listView)
+        controller.listView = listView
+        
+        coordinator?.rootViewControler = controller
+        controller.viewModel = viewModel
+        controller.viewDidAppearEvent = viewModel.loadCartItems
+        controller.coordinator = coordinator as? ListContentCoordinable
+        controller.view = mosaicComposerView.baseView
+        return controller
     }
 }
