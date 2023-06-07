f80b37a9790433f64b1edd20397704a1e3d60265
Tiago Amaral <tiagofly@outlook.com>
2023-06-07 19:21:35 -0300

    Update AlertViewFactoryTests.swift Teste na criacao de alertas.

---
+++ source/test/apptests/Shared/Factories/AlertViewFactoryTests.swift
@@ -0,0 +1,27 @@
+//
+//  AlertViewFActory.swift
+//  marvel-storeTests
+//
+//  Created by Tiago Amaral on 07/06/23.
+//  Copyright © 2023 developer_organization_name. All rights reserved.
+//
+
+import XCTest
+
+final class AlertViewFactoryTests: XCTestCase {
+
+    let valueTitleVerification: String = "alert_test_title_value"
+    let valueMessageVerification: String = "alert_test_message_value"
+
+    func testMakeAlertViewController() {
+        
+        let alertController = AlertViewFactory.makeAlert(with: valueTitleVerification,
+                                                         and: valueMessageVerification,
+                                                         handler: nil)
+        
+        XCTAssertTrue(type(of: alertController) == UIAlertController.self, "Expect instance of class UIAlertController")
+        XCTAssertEqual(alertController.title, valueTitleVerification, "Expect same title")
+        XCTAssertEqual(alertController.message, valueMessageVerification, "Expect same message")
+
+    }
+}
