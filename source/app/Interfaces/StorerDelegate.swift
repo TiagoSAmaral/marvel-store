47242b00a35481718cfd966c6cb51ff85eb2b885
Tiago Amaral <tiagofly@outlook.com>
2023-06-11 20:57:07 -0300

    Update StorerDelegate.swift

---
+++ source/app/Interfaces/StorerDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  StorerDelegate.swift
+//  marvel-store
+//
+//  Created by Tiago Amaral on 11/06/23.
+//  Copyright © 2023 developer_organization_name. All rights reserved.
+//
+
+import Foundation
+
+protocol StorerDelegate: AnyObject {
+    func listItems(from collection: ViewModelBehavior.Type) -> [ViewModelBehavior]?
+    func save(item: ViewModelBehavior?, into collection: ViewModelBehavior.Type)
+    func remove(item: ViewModelBehavior?, from collection: ViewModelBehavior.Type)
+    func markItemFromApi(item: ViewModelBehavior?, from collection: ViewModelBehavior.Type)
+}
