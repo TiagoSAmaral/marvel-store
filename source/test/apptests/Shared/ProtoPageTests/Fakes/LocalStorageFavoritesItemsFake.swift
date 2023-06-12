b78cc12e61a9b1a238065966f11b092096d3963d
Tiago Amaral <tiagofly@outlook.com>
2023-06-12 18:57:15 -0300

    Update LocalStorageFavoritesItemsFake.swift Captura de item enviado para os métodos de LocalStorageItemsFake,  para validar execução dentro de ListContentPageViewModelTests

--- source/test/apptests/Shared/ProtoPageTests/Fakes/LocalStorageFavoritesItemsFake.swift
+++ source/test/apptests/Shared/ProtoPageTests/Fakes/LocalStorageFavoritesItemsFake.swift
@@ -9,16 +9,25 @@
 import Foundation
 
 final class LocalStorageFavoritesItemsFake: StorerDelegate {
+    
+    var savedItem: ViewModelBehavior?
+    var removedItem: ViewModelBehavior?
+    var markedItem: ViewModelBehavior?
+    
     func listItems(from collection: ViewModelBehavior.Type) -> [ViewModelBehavior]? {
-        nil
+        MockerContentProvider().loadManyItem(type: Comic.self)?.compactMap({ $0.isFavorable = true; return $0 })
     }
     
     func save(item: ViewModelBehavior?, into collection: ViewModelBehavior.Type) {
+        savedItem = item
     }
     
     func remove(item: ViewModelBehavior?, from collection: ViewModelBehavior.Type) {
+        removedItem = item
     }
     
     func markItemFromApi(item: ViewModelBehavior?, from collection: ViewModelBehavior.Type) {
+        debugPrint("MARKED ITEM: \(item?.identifier)")
+        markedItem = item
     }
 }
