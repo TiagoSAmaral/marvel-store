03ee722d1042968df3ecdfa693f5eca302e9a045
Tiago Amaral <tiagofly@outlook.com>
2023-06-12 18:58:52 -0300

    Update ListContentPageViewModelTests.swift Implementado tests unitarios para carregar detalhes de um item,  carregar items da loja vindos da API,  salvar item como favorito e excluir item dos favoritos.

--- source/test/apptests/Shared/ProtoPageTests/ListContentPageViewModelTests.swift
+++ source/test/apptests/Shared/ProtoPageTests/ListContentPageViewModelTests.swift
@@ -9,19 +9,21 @@
 import XCTest
 
 final class ListContentPageViewModelTests: XCTestCase {
-
-    var out: ListContentPageViewModel?
+    
+    var out: ListContentPageViewModel = FactoryToTest.build()
     
     override func setUp() {
         out = FactoryToTest.build()
     }
     
     func testFilterYearDisableFilterValue() {
-        XCTAssertEqual(out?.disableFilterOptions, "Todos", "Expect be equal.")
+//        var out: ListContentPageViewModel = FactoryToTest.build()
+        XCTAssertEqual(out.disableFilterOptions, "Todos", "Expect be equal.")
     }
     
     func testListOfYears() {
-        guard let listYearFilter = out?.listYearFilter else {
+//        var out: ListContentPageViewModel = FactoryToTest.build()
+        guard let listYearFilter = out.listYearFilter else {
             XCTFail("Spect not nil")
             return
         }
@@ -34,42 +36,119 @@ final class ListContentPageViewModelTests: XCTestCase {
     }
     
     func testLoadDetailOfOneItem() {
-        
+//        var out: ListContentPageViewModel = FactoryToTest.build()
         let oneItem = MockerContentProvider().loadOneItem(type: Comic.self)
-        out?.selectedItem = oneItem
-        out?.loadDetailOfItem()
+        out.selectedItem = oneItem
+        out.loadDetailOfItem()
         
-        guard let networkFake = out?.network as? NetworkContentOperationFake, let
+        guard let networkFake = out.network as? NetworkContentOperationFake, let
         requestParamIntercepted = networkFake.paramRequestIntercepted else {
             XCTFail("Expect NetworkContentOperationFake instance")
             return
         }
-        XCTAssertEqual(out?.currentPage, .zero, "Expected page zero")
+        XCTAssertEqual(out.currentPage, .zero, "Expected page zero")
         XCTAssertEqual(requestParamIntercepted.layoutView, .detailContentLayoutCard, "Expected `.detailContentLayoutCard` value.")
         XCTAssertEqual(requestParamIntercepted.identifier, oneItem?.identifier, "Expected same identifier of selectedItem into RequestParam.")
     }
     
     func testLoadStoreItems() {
-        out?.currentPage = 3
-        out?.currentSearchValue = "Avengers"
-        out?.selectedFilterOption = "2006"
+//        var out: ListContentPageViewModel = FactoryToTest.build()
+        out.currentPage = 3
+        out.currentSearchValue = "Avengers"
+        out.selectedFilterOption = "2006"
         
-        out?.loadStoreItems()
+        out.loadStoreItems()
         
-        guard let networkFake = out?.network as? NetworkContentOperationFake, let
+        guard let networkFake = out.network as? NetworkContentOperationFake, let
         requestParamIntercepted = networkFake.paramRequestIntercepted else {
             XCTFail("Expect NetworkContentOperationFake instance")
             return
         }
         
-        XCTAssertEqual(out?.currentPage, .zero, "Expected page zero")
-        XCTAssertEqual(out?.currentSearchValue, nil, "Expect to be nil")
-        XCTAssertEqual(out?.selectedFilterOption, nil, "Expect to be nil")
+        XCTAssertEqual(out.currentPage, .zero, "Expected page zero")
+        XCTAssertEqual(out.currentSearchValue, nil, "Expect to be nil")
+        XCTAssertEqual(out.selectedFilterOption, nil, "Expect to be nil")
         XCTAssertEqual(requestParamIntercepted.sincePage, .zero, "Expected param page zero")
         XCTAssertEqual(requestParamIntercepted.layoutView, .listContentLayoutCard, "Expected param layout item `.listContentLayoutCard` value")
     }
     
-    func testSaveItemToFavorite() {
-        out?.currentViewModelStrategy = .detail
+    func testSaveItemToFavoriteOnDetailView() {
+        var outInner: ListContentPageViewModel = FactoryToTest.build(networkTypeResponse: .successOne)
+        let oneItem = MockerContentProvider().loadOneItem(type: Comic.self)
+        let requestOne = MockerContentProvider().loadOneContent(type: GeneralResult<Comic>.self)
+        outInner.currentViewModelStrategy = .detail
+        outInner.selectedItem = oneItem
+        
+        // Load is necessary to (simulate) charge items with API content
+        outInner.loadDetailOfItem()
+        
+        let expectRunRequest = expectation(description: "Expect run async request")
+        
+        DispatchQueue.main.async {
+            expectRunRequest.fulfill()
+            outInner.saveToFavorite(oneItem)
+            
+            guard let storageFavoriteFake = outInner.localStorageFavoriteItems as? LocalStorageFavoritesItemsFake else {
+                XCTFail("Expected LocalStorageFavoritesItemsFake instance")
+                return
+            }
+            
+            guard let savedItem = storageFavoriteFake.savedItem else {
+                XCTFail("Expected saveItem instance captured inside fake")
+                return
+            }
+            
+            guard let marketItem = storageFavoriteFake.markedItem else {
+                XCTFail("Expected markedItem instance captured inside fake")
+                return
+            }
+            
+            XCTAssertEqual(savedItem.identifier, oneItem?.identifier, "Expect same identifier")
+            XCTAssertEqual(outInner.items.count, 1, "Expect list of items nil")
+            XCTAssertEqual(marketItem.identifier, (outInner.selectedItem as? ViewModelBehavior)?.identifier, "Expected same identifier of selected item" )
+            
+        }
+       
+        wait(for: [expectRunRequest])
+    }
+    
+    func testRemoveItemFromFavoriteOnDetailView() {
+        var outInner: ListContentPageViewModel = FactoryToTest.build(networkTypeResponse: .successOne)
+        let oneItem = MockerContentProvider().loadOneItem(type: Comic.self)
+        let requestOne = MockerContentProvider().loadOneContent(type: GeneralResult<Comic>.self)
+        outInner.currentViewModelStrategy = .detail
+        outInner.selectedItem = oneItem
+        
+        // Load is necessary to (simulate) charge items with API content
+        outInner.loadDetailOfItem()
+        
+        let expectRunRequest = expectation(description: "Expect run async request")
+        
+        DispatchQueue.main.async {
+            expectRunRequest.fulfill()
+            outInner.removeFavorite(oneItem)
+            
+            guard let storageFavoriteFake = outInner.localStorageFavoriteItems as? LocalStorageFavoritesItemsFake else {
+                XCTFail("Expected LocalStorageFavoritesItemsFake instance")
+                return
+            }
+            
+            guard let savedItem = storageFavoriteFake.removedItem else {
+                XCTFail("Expected saveItem instance captured inside fake")
+                return
+            }
+            
+            guard let marketItem = storageFavoriteFake.markedItem else {
+                XCTFail("Expected markedItem instance captured inside fake")
+                return
+            }
+            
+            XCTAssertEqual(savedItem.identifier, oneItem?.identifier, "Expect same identifier")
+            XCTAssertEqual(outInner.items.count, 1, "Expect list of items nil")
+            XCTAssertEqual(marketItem.identifier, (outInner.selectedItem as? ViewModelBehavior)?.identifier, "Expected same identifier of selected item" )
+            
+        }
+       
+        wait(for: [expectRunRequest])
     }
 }
